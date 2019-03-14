0e5f430ab921f18fbf70ec67563551b1586a5da1
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-14 21:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deepestChild()
   // reads the innerHTML of the deepest child
   //return document.querySelector('#grand-node div div div div').innerHTML;
   var grand = document.querySelector('#grand-node');
-  var divChain = grand.children[0].children[0].children[0].children[0].innerHTML;
+  var divChain = grand.children[0].children[0].children[0].children[0];
   
   return divChain;
   
