5a270c5e6dc052a692417b57c9de18216d74e89f
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-14 21:05:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getFirstSelector(selector)
 function nestedTarget()
 {
     //return document.querySelector('div#target[0][0][0] div.target')
-    return document.querySelector('#target div.target');
+    return document.querySelector('#nested div.target');
   
 }
 
