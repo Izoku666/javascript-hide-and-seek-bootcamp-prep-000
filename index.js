5e117d016aa4fc3aad4d81f44f46881d9224194b
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-14 21:23:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function deepestChild()
 function increaseRankBy(n)
 {
   // increases the ranks in the .ranked-list by n
-  var list = document.querySelectorAll('div.ranked-list > li');
+  var list = document.querySelectorAll('div.ranked-list > li').innerHTML;
   for(let i = 0; i < list.length; i++)
   {
-    list[i].innerHTML = parseInt(list[i]) + n;
+    list[i] = parseInt(list[i]) + n;
   }
   return list;
 
