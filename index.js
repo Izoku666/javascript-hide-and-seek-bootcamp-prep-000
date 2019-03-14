ecba650bae9bdc162201811d605106056bc01f26
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-14 20:54:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector);
+}
+
+function nestedTarget()
+{
+    
+}
+
+function deepestChild()
+{
+  // reads the innerHTML of the deepest child
+}
+
+function increaseRankBy(n)
+{
+  // increases the ranks in the .ranked-list by n
+}
 
